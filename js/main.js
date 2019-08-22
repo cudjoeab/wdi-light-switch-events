2abaf7eade71ae270eccb5dec39312a15f68ebbd
cudjoeab <cudjoeab@gmail.com>
2019-08-22 16:56:22 -0400

    completed assignment with toggle

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
+// When someone hits the button in the switch, 
+// remove the current state class (on/off), and 
+// apply the opposite class (on/off)
+let lightSwitch = document.querySelector('.plate');
+let roomStatus = document.querySelector('.light'); 
+let message = document.querySelector('.status');
+let classes = message.classList; 
+
+lightSwitch.addEventListener('click', function() {
+    console.log('switch clicked'); 
+    
+    if(roomStatus.classList.toggle('dark')){
+        message.textContent = "Who turned out the lights?!"
+    } 
+    if(roomStatus.classList.toggle('light')){
+        message.textContent = "It's so bright in here!"
+    }
+    
+
+
+})
 
 
